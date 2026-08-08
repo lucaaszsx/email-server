d2ef6390132d33520199d20cad6937d112ea5fb2
Lucas <jlucasmelob558@gmail.com>
2026-08-08 01:15:17 -0300

    refactor: use cache in logger to doesnt create a new instance in every access

--- server/src/decorators/InjectLogger.ts
+++ server/src/decorators/InjectLogger.ts
@@ -1,10 +1,15 @@
 import { Logger } from "../lib/logger/index.js";
+import { fileURLToPath } from "node:url";
 
-export function InjectLogger(filename: string) {
+export function InjectLogger(importMeta: string) {
+    const filename = fileURLToPath(importMeta);
+
     return function (target: any, propertyKey: string) {
+        const sym = Symbol(propertyKey);
         Object.defineProperty(target, propertyKey, {
             get() {
-                return new Logger(filename);
+                if (!this[sym]) this[sym] = new Logger(filename);
+                return this[sym];
             },
             enumerable: true,
             configurable: true
