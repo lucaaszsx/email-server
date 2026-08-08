36ebfa3e2674f95e041657e81a11938307fcbb4f
Lucas <jlucasmelob558@gmail.com>
2026-08-08 01:19:36 -0300

    chore: use tsyringe and setup middlewares

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,10 @@
-import { Env } from './config/env.js';
-import express from 'express';
+import 'reflect-metadata';
+
+import { ErrorHandlerMiddleware, NotFoundMiddleware } from './middlewares/index.js';
 import { Logger } from './lib/logger/index.js';
+import { Env } from './config/env.js';
+import { container } from 'tsyringe';
+import express from 'express';
 
 async function bootstrap(): Promise<void> {
     const logger = new Logger();
@@ -10,7 +14,8 @@ async function bootstrap(): Promise<void> {
     app.listen(Env.port, () => {
         logger.info('Server listening on port %d', Env.port);
 
-        
+        app.use(container.resolve(NotFoundMiddleware).handler);
+        app.use(container.resolve(ErrorHandlerMiddleware).handler);
     });
 }
 
